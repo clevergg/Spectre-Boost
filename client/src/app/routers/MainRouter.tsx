6eeb352bdbb76a5a32555e9ceaf3baa38758f5b1
RENEE <wind.autumn@mail.ru>
2026-03-30 19:36:37 +0300

    fix error boundary

--- client/src/app/routers/MainRouter.tsx
+++ client/src/app/routers/MainRouter.tsx
@@ -14,7 +14,7 @@ import { ScrollHandler } from "../../shared/ui/ScrollHandler"
 import { usePromoFromUrl } from "../../core/hooks/usePromoFromUrl"
 import SpectreFallback from "../../shared/ui/SpectreFallback"
 import { routes } from "../config/routes"
-import { ErrorBoundary } from "../../components/ErrorBoundary"
+// import { ErrorBoundary } from "../../components/ErrorBoundary"
 
 const HomePage = lazy(() => import("../../pages/HomeRoute"))
 const ServicesPage = lazy(() => import("../../pages/ServicesRoute"))
@@ -34,30 +34,30 @@ export const MainRouter = () => {
   return (
     <Router>
       <AppInit />
-      <ErrorBoundary>
-        <Suspense fallback={<SpectreFallback />}>
-          <ScrollHandler />
-          <Routes>
-            <Route element={<DefaultLayout />}>
-              <Route index element={<HomePage />} />
-              <Route path={routes.services} element={<ServicesPage />} />
-              <Route path={routes.aboutus} element={<AboutUsPage />} />
-              <Route path={routes.survivor} element={<SurvivorPage />} />
-            </Route>
+      {/* <ErrorBoundary> */}
+      <Suspense fallback={<SpectreFallback />}>
+        <ScrollHandler />
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path={routes.services} element={<ServicesPage />} />
+            <Route path={routes.aboutus} element={<AboutUsPage />} />
+            <Route path={routes.survivor} element={<SurvivorPage />} />
+          </Route>
 
-            {/* Защищённые роуты — требуют авторизации */}
-            <Route
-              element={
-                <ProtectedRoute>
-                  <UserAccountLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route path={routes.account} element={<AccountPage />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </ErrorBoundary>
+          {/* Защищённые роуты — требуют авторизации */}
+          <Route
+            element={
+              <ProtectedRoute>
+                <UserAccountLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route path={routes.account} element={<AccountPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
+      {/* </ErrorBoundary> */}
     </Router>
   )
 }
